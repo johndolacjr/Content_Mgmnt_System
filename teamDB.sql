742822eb9f53bafa695aa7dcb5c2c8879f54c27b
johndolac <johndolacjr@gmail.com>
2020-12-04 22:06:25 -0700

    attempted coding tables in schema

--- teamDB.sql
+++ teamDB.sql
@@ -2,23 +2,22 @@
 DROP DATABASE IF EXISTS teamDB;
 -- Creates the "teamDB" database --
 CREATE DATABASE teamDB;
-
 -- Makes it so all of the following code will affect teamDB --
 USE teamDB;
 
--- Creates the table "Team_Table" within teamDB --
-CREATE TABLE Department_table (
-  -- Creates Dept. Unique ID --
-  id INT PRIMARY KEY NOT NULL,
-  -- Contains the Department Name --
-  Department_Name VARCHAR(30) NOT NULL
+-- Creates the table "department_table" within teamDB --
+CREATE TABLE Department_table(
+  id INTEGER AUTO_INCREMENT,
+  Department_Name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id) NOT NULL
 );
 
-CREATE TABLE Role_table (
-    id INT PRIMARY KEY
+CREATE TABLE Role_table(
+    id INTEGER AUTO_INCREMENT NOT NULL, 
     title VARCHAR(30) to hold role title
     salary DECIMAL to hold role salary
-    department_id INT to hold reference to department role belongs to
+    department_id INT to hold reference to department role belongs to,
+    PRIMARY KEY (id) NOT NULL
 );
 
 CREATE TABLE Employee_table (
@@ -28,3 +27,19 @@ CREATE TABLE Employee_table (
     role_id INT to hold reference to role employee has
     manager_id INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
 );
+
+-- INSERT INTO Role_table (id) values ((id));
+-- INSERT INTO Role_table (title) values ('Manager', 'Sales Rep', 'Engineer', 'Payment Analyst');
+-- INSERT INTO Role_table (salary) values (???????????);
+-- INSERT INTO Role_table () values ((id));
+
+
+-- INSERT INTO books (title, authorId) values ('Pride and Prejudice', 1);
+-- INSERT INTO books (title, authorId) values ('Emma', 1);
+-- INSERT INTO books (title, authorId) values ('The Adventures of Tom Sawyer', 2);
+-- INSERT INTO books (title, authorId) values ('Adventures of Huckleberry Finn', 2);
+-- INSERT INTO books (title, authorId) values ('Alice''s Adventures in Wonderland', 3);
+-- INSERT INTO books (title, authorId) values ('Dracula', null);
+
+-- SELECT * FROM authors;
+-- SELECT * FROM books;
