79ac6265ef962e55943fa4fa65e0f42f880d89f4
johndolac <johndolacjr@gmail.com>
2020-12-06 12:34:38 -0700

    cleaned up schema code

--- teamDB.sql
+++ teamDB.sql
@@ -2,44 +2,45 @@
 DROP DATABASE IF EXISTS teamDB;
 -- Creates the "teamDB" database --
 CREATE DATABASE teamDB;
+
 -- Makes it so all of the following code will affect teamDB --
 USE teamDB;
 
--- Creates the table "department_table" within teamDB --
-CREATE TABLE Department_table(
-  id INTEGER AUTO_INCREMENT,
-  Department_Name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id) NOT NULL
+-- Creates the table "Team_Table" within teamDB --
+CREATE TABLE Department_table (
+  id INT PRIMARY KEY NOT NULL,
+  DepartmentName VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE Role_table(
-    id INTEGER AUTO_INCREMENT NOT NULL, 
-    title VARCHAR(30) to hold role title
-    salary DECIMAL to hold role salary
-    department_id INT to hold reference to department role belongs to,
-    PRIMARY KEY (id) NOT NULL
+CREATE TABLE Role_table (
+    id INT PRIMARY KEY NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    departmentId INT
 );
 
 CREATE TABLE Employee_table (
-    id INT PRIMARY KEY
-    first_name VARCHAR(30) to hold employee first name
-    last_name VARCHAR(30) to hold employee last name
-    role_id INT to hold reference to role employee has
-    manager_id INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
+    id INT PRIMARY KEY NOT NULL,
+    firstName VARCHAR(30),
+    lastName VARCHAR(30), 
+    roleId INT,
+    managerId INT
 );
 
--- INSERT INTO Role_table (id) values ((id));
--- INSERT INTO Role_table (title) values ('Manager', 'Sales Rep', 'Engineer', 'Payment Analyst');
--- INSERT INTO Role_table (salary) values (???????????);
--- INSERT INTO Role_table () values ((id));
+INSERT INTO Department_table (id) VALUES ("");
+INSERT INTO Department_table (DepartmentName) VALUES ("Sales"), ("Marketing"), ("Development"), ("Supplier Services");
 
+INSERT INTO Role_table (id) VALUES ("");
+INSERT INTO Role_table (title) VALUES ("Manager"), ("Sales Rep"), ("Developer"), ("Customer Service Rep");
+INSERT INTO Role_table (salary) VALUES ("$75K"), ("$65K"), ("$90K"), ("$45K");
+INSERT INTO Role_table (departmentId) VALUES ("");
 
--- INSERT INTO books (title, authorId) values ('Pride and Prejudice', 1);
--- INSERT INTO books (title, authorId) values ('Emma', 1);
--- INSERT INTO books (title, authorId) values ('The Adventures of Tom Sawyer', 2);
--- INSERT INTO books (title, authorId) values ('Adventures of Huckleberry Finn', 2);
--- INSERT INTO books (title, authorId) values ('Alice''s Adventures in Wonderland', 3);
--- INSERT INTO books (title, authorId) values ('Dracula', null);
+INSERT INTO Employee_table (id) VALUES ("");
+INSERT INTO Employee_table (firstName) VALUES ("first name");
+INSERT INTO Employee_table (lastName) VALUES ("last name");
+INSERT INTO Employee_table (roleId) VALUES ("role id");
+INSERT INTO Employee_table (managerId) VALUES ("manager id");
 
--- SELECT * FROM authors;
--- SELECT * FROM books;
+SELECT * FROM Department_table;
+SELECT * FROM Role_table;
+SELECT * FROM Employee_table;
